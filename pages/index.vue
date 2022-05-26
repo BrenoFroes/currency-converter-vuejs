0dc3ba2901a322b3af86e4e95dcce159a9b3e2f9
Breno A Froes P <breno.froes@timeprimo.com>
2022-05-26 15:10:39 -0300

    [feature] Visual asset

--- pages/index.vue
+++ pages/index.vue
@@ -11,26 +11,21 @@
       <button type="submit" class="bg-blue-300 hover:bg-blue-500 text-white font-bold py-2 px-4 rounded mr-5">Add</button>
     </form>
     <hr>
-    <button v-on:click="loadListCripts()">Load criptos</button>
-    {{ criptoList }}
-    <table class="w-full text-left">
+    <table class="w-full text-left border-solid border-2 border-sky-500 rounded mt-5">
       <thead>
         <tr>
           <th>Name</th>
+          <th>ID</th>
           <th>Value</th>
           <th>USD ($)</th>
         </tr>
       </thead>
-      <tbody>
+      <tbody v-for="cripto in criptoList">
         <tr>
-          <td>Bitcoin</td>
+          <td> {{ cripto.name }} </td>
+          <td> {{ cripto.asset_id }} </td>
           <td>1</td>
-          <td>29.370</td>
-        </tr>
-        <tr>
-          <td>Etherum</td>
-          <td>1</td>
-          <td>1.855</td>
+          <td> {{ cripto.price_usd }} </td>
         </tr>
       </tbody>
     </table>
@@ -38,29 +33,42 @@
 </template>
 
 <script>
-export default {
-  name: 'IndexPage',
-  data() {
-    return {
-      criptoList: []
-    }
-  },
-  methods: {
-    loadListCripts() {
-      this.$axios
-        .$get("https://rest.coinapi.io/v1/assets?filter_asset_id=BTC,ETH,USDT,BNB,USD,XRP,LUNA,ADA,SOL,BUSD", {
-          headers: {
-            'X-CoinAPI-Key': 'C0012C6B-347D-4FE3-9F48-11A3E53FEDAE'
-          }
-        })
-        .then((response) => {
-          console.log(response);
-          this.criptoList = response;
-        })
-        .catch(function (error) {
-          console.log(error);
-        });
+  export default {
+    name: 'IndexPage',
+    data () {
+      return {
+        criptoList: [],
+        namesList: ["BTC","ETH","USDT","BNB","USDC","XRP","LUNA","ADA","SOL","BUSD"],
+        transformList: ""
+      }
     },
-  },
-}
+    methods: {
+      loadListCripts() {
+        this.$axios
+          .$get(`https://rest.coinapi.io/v1/assets?filter_asset_id=${this.transformList}`, {
+            headers: {
+              'X-CoinAPI-Key': 'C0012C6B-347D-4FE3-9F48-11A3E53FEDAE'
+            }
+          })
+          .then((response) => {
+            console.log(response);
+            this.criptoList = response;
+          })
+          .catch(function (error) {
+            console.log(error);
+          });
+      },
+      adaptedList(){
+        this.transformList = this.namesList.toString();
+        this.transformList = this.transformList.replace('"','');
+        this.transformList = this.transformList.replace('[','');
+        this.transformList = this.transformList.replace(']','');
+        console.log(this.adaptedList);
+      }
+    }, 
+    mounted: function() {
+      this.adaptedList();
+      this.loadListCripts();  
+    }
+  }
 </script>
