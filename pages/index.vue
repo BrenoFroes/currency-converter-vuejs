43efcd7ecf3c17de4d604068ccf8b8fc1c34e628
Breno A Froes P <breno.froes@timeprimo.com>
2022-05-27 17:27:44 -0300

    [bugFix] Adjust of order list

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,15 @@
 <template>
   <div class="w-full md:w-3/4 mx-5 md:mx-auto my-20">
     <CustomHeader></CustomHeader>
-    <div class="flex items-center justify-start items-start flex-col sm:flex-row mt-10 mb-5">
+    <div class="flex items-center justify-start items-start flex-col lg:flex-row mt-10 mb-5">
+      <form id="api-key" class="w-auto mt-5 sm:mt-0 flex flex-col sm:flex-row mr-auto sm:items-center"
+        @submit.prevent="passKey()">
+        <label class="mr-5" for="cripto-name">API key:</label>
+        <input type="text" class="border-solid border-2 mr-5" placeholder="ex: C826E5FE-B498-4874-83AB-D89B63B1D71F"
+          id="cripto-name" v-model="apiKey">
+        <button type="submit"
+          class="bg-green-500 hover:bg-green-300 text-white font-bold sm:py-2 sm:px-4 py-1 px-2 rounded mt-5 sm:mt-2 justify-start mr-0 sm:mr-5">Validate</button>
+      </form>
       <form id="usd-value" class="w-auto mt-5 sm:mt-0 flex flex-col sm:flex-row mr-auto sm:items-center">
         <label class="mr-5" for="cripto-name">USD value:</label>
         <input type="text" class="border-solid border-2 mr-5" placeholder="ex: 20.00" id="cripto-name"
@@ -28,18 +36,18 @@
         <tr>
           <th>Name</th>
           <th>ID</th>
-          <th>coin/$</th>
           <th v-if="usdValue">USD ($)</th>
+          <th>USD/coin</th>
           <th v-if="usdValue">Coins</th>
         </tr>
       </thead>
-      <tbody v-for="(item, index) in criptoList">
+      <tbody v-for="(item, index) in newQuoteList">
         <tr>
           <td> {{ item.name }} </td>
-          <td> {{ item.asset_id }} </td>
-          <td> {{ quoteList[index] }} </td>
+          <td> {{ item.id }} </td>
           <td v-if="usdValue"> {{ usdValue }} </td>
-          <td v-if="usdValue"> {{ newQuoteList[index] }} </td>
+          <td> {{ item.quote }} </td>
+          <td v-if="usdValue"> {{ item.result }} </td>
         </tr>
       </tbody>
     </table>
@@ -55,12 +63,12 @@ export default {
     return {
       svgLoading: require("../assets/svg/bars.svg"),
       svgError: require("../assets/svg/error.svg"),
-      coinKey: "DF0FBFA7-62B3-48EE-AC9E-E9AEDDF7592F",
+      coinKey: "C826E5FE-B498-4874-83AB-D89B63B1D71F",
+      apiKey: "",
       namesList: ["BTC", "ETH", "USDT", "BNB", "USDC", "XRP", "LUNA", "ADA", "SOL", "BUSD"],
       newCripto: "",
       newQuoteList: [],
       criptoList: [],
-      quoteList: [],
       usdValue: null,
       erro: false,
       loading: false
@@ -91,9 +99,16 @@ export default {
             this.erro = false;
             this.criptoList = response;
             for (let i = 0;i < this.criptoList.length;i++) {
-              // Set calculated value in 1$ and push to array
-              let resultOne = 1 / this.criptoList[i].price_usd;
-              this.quoteList.push(resultOne);
+              // Set calculated value in infomated USD
+              let newObject = {
+                name: `${this.criptoList[i].name}`,
+                id: `${this.criptoList[i].asset_id}`,
+                quote: `${1 / this.criptoList[i].price_usd}`,
+                result: `${this.usdValue / this.criptoList[i].price_usd}`
+              }
+              if (this.newQuoteList.length < this.criptoList.length) {
+                this.newQuoteList.push(newObject);
+              }
             }
           }
           else {
@@ -126,15 +141,15 @@ export default {
             let sizeArray = this.criptoList.length - 1;
             this.namesList.push(this.criptoList[sizeArray].asset_id);
 
-            // Set calculated value in 1$
-            let resultNew = 1 / this.criptoList[sizeArray].price_usd;
-
             // Set calculated value in infomated USD
-            let resultNewUSD = this.usdValue / this.criptoList[sizeArray].price_usd;
+            let newObjectUSD = {
+              name: `${response[0].name}`,
+              id: `${response[0].asset_id}`,
+              quote: `${1 / this.criptoList[sizeArray].price_usd}`,
+              result: `${this.usdValue / this.criptoList[sizeArray].price_usd}`
+            }
 
-            // Push values in arrays
-            this.newQuoteList.push(resultNewUSD);
-            this.quoteList.push(resultNew);
+            this.newQuoteList.push(newObjectUSD);
           }
           else {
             this.erro = true;
@@ -148,8 +163,20 @@ export default {
     },
     convertValues() {
       for (let i = 0;i < this.criptoList.length;i++) {
-        let result = this.usdValue / this.criptoList[i].price_usd;
-        this.newQuoteList[i] = result;
+        // Set calculated value in infomated USD
+        let newObjectUSD = {
+          name: `${this.criptoList[i].name}`,
+          id: `${this.criptoList[i].asset_id}`,
+          quote: `${1 / this.criptoList[[i]].price_usd}`,
+          result: `${this.usdValue / this.criptoList[i].price_usd}`
+        }
+        this.newQuoteList[i] = newObjectUSD;
+      }
+      this.loadListCripts();
+    },
+    passKey(){
+      if(this.apiKey){
+        this.coinKey = this.apiKey;
       }
       this.loadListCripts();
     }
